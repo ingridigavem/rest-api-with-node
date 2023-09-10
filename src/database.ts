126ff3cd82ebd2c53423546e15f6ac0d40c7350f
Ingridi Gave <ingridigavem@gmail.com>
2023-09-10 08:08:36 -0300

    inclusao de dois bancos na aplicacao

--- src/database.ts
+++ src/database.ts
@@ -2,10 +2,11 @@ import { Knex, knex as setupKnex } from 'knex';
 import { env } from './env';
 
 export const config: Knex.Config = {
-  client: 'sqlite',
-  connection: {
-    filename: env.DATABASE_URL,
-  },
+  client: env.DATABASE_CLIENT,
+  connection:
+    env.DATABASE_CLIENT === 'sqlite'
+      ? { filename: env.DATABASE_URL }
+      : env.DATABASE_URL,
   useNullAsDefault: true, // por padrao todos os campos do banco terao valor nulo - necessario para o sqlite
   migrations: {
     extension: 'ts',
